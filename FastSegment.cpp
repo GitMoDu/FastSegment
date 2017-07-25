b8e2b926ed205de57fd322c938a1bfd5c464d160
André Pereira <deltazero@outlook.com>
2017-07-25 11:36:13 +0100

    Optimization, clean-up and documentation.

--- FastSegment.cpp
+++ FastSegment.cpp
@@ -1,16 +1,17 @@
+
 /// 
 ///  Created for personal use, use it at your own risk and benefit.
+///  https://github.com/GitMoDu/FastSegment
 /// 
 
 #include "FastSegment.h"
 
-
-bool FastSegment::Setup(const uint8_t dataPin, const uint8_t clockPin, const uint8_t shiftPin, const uint8_t digitCount)
+bool FastSegment::Setup(const byte dataPin, const byte clockPin, const byte shiftPin, const byte digitCount)
 {
-	if (dataPin < ARDUINO_MAX_PIN
-		&& clockPin < ARDUINO_MAX_PIN
-		&& shiftPin < ARDUINO_MAX_PIN
-		&& digitCount <= MAX_DIGIT_COUNT)
+	if (dataPin < NUM_DIGITAL_PINS
+		&& clockPin < NUM_DIGITAL_PINS
+		&& shiftPin < NUM_DIGITAL_PINS
+		&& digitCount <= NUM_DIGITAL_PINS)
 	{
 		DataPin = dataPin;
 		ClockPin = clockPin;
@@ -32,25 +33,140 @@ bool FastSegment::Setup(const uint8_t dataPin, const uint8_t clockPin, const uin
 	}
 }
 
-uint32_t FastSegment::GetLastNumber()
+byte FastSegment::GetLastNumber()
 {
 	return LastNumber;
 }
 
-//void FastSegment::BootUpAnimation()
-//{
-//	Clear();
-//	uint64_t NextRefresh = micros();
-//	uint64_t Elapsed = micros();
-//	NextRefresh = 0;
-//	Demo();
-//
-//}
-
-
-byte FastSegment::GetNumberByte(const uint8_t number0to9, const bool decimalOn)
+void FastSegment::SetDigitsAll(const byte value)
 {
+	for (byte i = 0; i < DigitCount; i++)
+	{
+		Digits[i] = value;
+	}
+}
 
+
+bool FastSegment::SetNumber(const uint32_t number, const bool replaceZero)
+{
+	if (number != LastNumber)
+	{
+		LastNumber = number;
+
+		byte IntValue;
+		bool ReachedMSD = false;
+		for (char i = DigitCount - 1; i >= 0; i--)
+		{
+			IntValue = ((number / TenPower((byte)i)) % 10);
+
+			if (!ReachedMSD)
+			{
+				ReachedMSD = IntValue > 0;
+			}
+
+			if (ReachedMSD)
+			{
+				Digits[i] = GetDigitByte(IntValue);
+			}
+			else
+			{
+				Digits[i] = SEGMENT_CLEAR;
+			}
+		}
+		return true;
+	}
+	else
+	{
+		return false;
+	}	
+}
+
+void FastSegment::ShowBytes(byte byteArray[], byte byteArraySize)
+{
+	for (byte i = 0; i < byteArraySize; i++)
+	{
+		WriteByte(byteArray[i]);
+	}
+
+	Shift.PulseLow();
+}
+
+void FastSegment::ShowNumber()
+{
+	ShowBytes(Digits, DigitCount);
+}
+
+void FastSegment::Reset()
+{
+	Data.Set(HIGH);
+	Clock.PulseLow();
+	Data.Set(LOW);
+	Shift.PulseLow();
+}
+
+void FastSegment::SetAll(const bool bitValue)
+{
+	Data.Set(bitValue);
+	for (byte i = 0; i < DigitCount * BYTE_SIZE; i++)
+	{
+			Clock.PulseLow();
+	}
+	Data.Set(LOW);
+	Shift.PulseLow();
+}
+void FastSegment::Clear()
+{
+	SetAll(HIGH);
+}
+
+void FastSegment::SetHead(const bool value)
+{
+	Data.Set(value);
+	Clock.PulseLow();
+	Data.Set(LOW);
+}
+
+void FastSegment::WriteByte(const byte value)
+{
+	for (int i = 0; i < BYTE_SIZE; i++)
+	{
+		SetHead((value)&(1 << (BYTE_SIZE - 1 - i)));
+	}
+}
+
+uint32_t FastSegment::TenPower(const byte power)
+{
+	switch (power)
+	{
+	case 0:
+		return (uint32_t)1;
+	case 1:
+		return (uint32_t)10;
+	case 2:
+		return (uint32_t)100;
+	case 3:
+		return (uint32_t)1000;
+	case 4:
+		return (uint32_t)10000;
+	case 5:
+		return (uint32_t)100000;
+	case 6:
+		return (uint32_t)1000000;
+	case 7:
+		return (uint32_t)10000000;
+	case 8:
+		return (uint32_t)100000000;
+	case 9:
+		return (uint32_t)1000000000;
+	case 10:
+		return (uint32_t)10000000000;
+	default:
+		return (uint32_t)0;
+	}
+}
+
+byte FastSegment::GetDigitByte(const byte number0to9, const bool decimalOn)
+{
 	if (number0to9 > 9)
 	{
 		return SEGMENT_ERROR;
@@ -107,180 +223,5 @@ byte FastSegment::GetNumberByte(const uint8_t number0to9, const bool decimalOn)
 			return SEGMENT_9;
 		}
 	}
-
-}
-
-void FastSegment::SetDigitsAll(const byte value)
-{
-	for (uint8_t i = 0; i < DigitCount; i++)
-	{
-		Digits[i] = value;
-	}
-}
-
-void FastSegment::SetDigits(const uint32_t value, const bool show)
-{
-	LastNumber = value;
-	for (uint8_t i = DigitCount - 1; i >= 0; i--)
-	{
-		Digits[i] = GetNumberByte(((value / TenPower(i)) % 10));
-
-		if (i < 1)
-		{
-			break;
-		}
-	}
-
-	if (show)
-	{
-		ShowDigits();
-	}
-}
-
-void FastSegment::SetDigitsReplaceMSZero(const uint32_t value, const byte replaceZero, const bool show)
-{
-	LastNumber = value;
-	bool ReachedMSB = false;
-	for (uint8_t i = DigitCount - 1; i >= 0; i--)
-	{
-		uint8_t intValue = ((value / TenPower((uint8_t)i)) % 10);
-		if (!ReachedMSB)
-		{
-			ReachedMSB = intValue > 0;
-
-			if (!ReachedMSB && intValue == 0)
-			{
-				Digits[i] = replaceZero;
-			}
-			else
-			{
-				Digits[i] = GetNumberByte(intValue);
-			}
-		}
-		else
-		{
-			Digits[i] = GetNumberByte(intValue);
-		}
-
-		if (i < 1)
-		{
-			break;
-		}
-	}
-
-	if (show)
-	{
-		//Clear();
-		ShowDigits();
-	}
-}
-
-void FastSegment::ShowDigits()
-{
-	for (uint8_t i = 0; i < DigitCount; i++)
-	{
-		WriteByte(Digits[i]);
-	}
-}
-
-void FastSegment::Reset()
-{
-	Data.Set(LOW);
-	Clock.Set(LOW);
-	Shift.Set(LOW);
-	Clock.Set(HIGH);
-	Shift.Set(HIGH);
-}
-
-
-void FastSegment::SetAll(const bool value)
-{
-	//ActiveLed.PulseHigh();
-
-	SetHead(value);
-	for (uint8_t i = 0; i < DigitCount; i++)
-	{
-		//ActiveLed.PulseHigh();
-		for (uint8_t j = 0; j < BYTE_SIZE; j++)
-		{
-			Clock.PulseLow();
-			Push();
-		}
-	}
-}
-void FastSegment::Clear()
-{
-	SetAll(HIGH);
-}
-
-void FastSegment::SetHead(const bool value)
-{
-	//ActiveLed.PulseHigh();
-
-	Data.Set(value);
-
-	Clock.PulseLow();
-}
-
-void FastSegment::Push(const uint8_t times)
-{
-	//ActiveLed.PulseHigh();
-
-	for (uint8_t i = 0; i < times; i++)
-	{
-		Shift.PulseLow();
-	}
-}
-
-
-void FastSegment::WriteByte(const byte value)
-{
-	//ActiveLed.PulseHigh();
-
-	for (int i = 0; i < BYTE_SIZE; i++)
-	{
-		SetHead((value)&(1 << (BYTE_SIZE - 1 - i)));
-		Push();
-	}
-}
-
-uint32_t FastSegment::TenPower(const uint8_t power)
-{
-	uint32_t Ten = 1;
-
-	switch (power)
-	{
-	case 0:
-		return (uint32_t)1;
-	case 1:
-		return (uint32_t)10;
-	case 2:
-		return (uint32_t)100;
-	case 3:
-		return (uint32_t)1000;
-	case 4:
-		return (uint32_t)10000;
-	case 5:
-		return (uint32_t)100000;
-	case 6:
-		return (uint32_t)1000000;
-	case 7:
-		return (uint32_t)10000000;
-	case 8:
-		return (uint32_t)100000000;
-	case 9:
-		return (uint32_t)1000000000;
-	case 10:
-		return (uint32_t)10000000000;
-	default:
-		while (true);;
-		break;
-	}
-	//Equivalent to this, up to 10 power and much, much faster.
-	//for (uint8_t i = 0; i < power; i++)
-	//{
-	//	Ten *= (uint32_t)10;
-	//}
-	//return Ten;
 }
 
--- FastSegment.h
+++ FastSegment.h
@@ -8,57 +8,52 @@
 #include <Arduino.h>
 #include <Fast.h>
 
-#define ARDUINO_MAX_PIN NUM_DIGITAL_PINS
 #define MAX_DIGIT_COUNT 10
 #define BYTE_SIZE 8
 
 class FastSegment
 {
 private:
-#define SEGMENT_ERROR B10101010//Error
+#define SEGMENT_ERROR B10101010
 
-	uint8_t DataPin, ClockPin, ShiftPin, DigitCount;
+	byte DataPin, ClockPin, ShiftPin, DigitCount;
 
-	FastShifter Clock, Shift;
-	//FastShifter ActiveLed;
 	FastOut Data;
+	FastShifter Clock, Shift;
 
-	uint32_t TenPower(const uint8_t power);
+	byte LastNumber = 0;
 
-	void Push(const uint8_t times = 1);
 	void SetHead(const bool value);
-	void SetAll(const bool value);
-	void Clear();
 	void Reset();
-	void WriteByte(const byte value);
-	uint32_t LastNumber = 0;
-	byte GetNumberByte(const uint8_t number1to9, const bool decimalOn = false);
+	void WriteByte(const byte value);	
 
+	//Equivalent to 10^power up to 10 power but much, much faster.
+	uint32_t TenPower(const byte power);
 
 public:
 
-	///////////////////B76543210
+///////////////////B76543210
 #define SEGMENT_0  B11000000 //All but 6 and 7
 #define SEGMENT_1  B11111001 //1 and 2
-#define SEGMENT_2  B10100100 //0, 1, 6, 4, 3
-#define SEGMENT_3  B10110000 //0, 1, 6, 2, 3
-#define SEGMENT_4  B10011001 //5, 6, 1, 2
-#define SEGMENT_5  B10010010 //0, 5, 6, 2, 3
-#define SEGMENT_6  B10000011 //5, 4, 3, 2, 6
+#define SEGMENT_2  B10100100 //0, 1, 3, 4, 6
+#define SEGMENT_3  B10110000 //0, 1, 2, 3, 6
+#define SEGMENT_4  B10011001 //1, 2, 5, 6
+#define SEGMENT_5  B10010010 //0, 2, 3, 5, 6
+#define SEGMENT_6  B10000011 //2, 3, 4, 5, 6
 #define SEGMENT_7  B11111000 //0, 1, 2
 #define SEGMENT_8  B10000000 //All but 7
-#define SEGMENT_9  B10011000 //0, 5, 6, 1, 2
+#define SEGMENT_9  B10011000 //0, 1, 2, 5, 6
 
-#define SEGMENT_0D  B01000000 //All but 6 and 7
-#define SEGMENT_1D  B01111001 //1 and 2
-#define SEGMENT_2D  B00100100 //0, 1, 6, 4, 3
-#define SEGMENT_3D  B00110000 //0, 1, 6, 2, 3
-#define SEGMENT_4D  B00011001 //5, 6, 1, 2
-#define SEGMENT_5D  B00010010 //0, 5, 6, 2, 3
-#define SEGMENT_6D  B00000011 //5, 4, 3, 2, 6
-#define SEGMENT_7D  B01111000 //0, 1, 2
-#define SEGMENT_8D  B00000000 //All but 7
-#define SEGMENT_9D  B00011000 //0, 5, 6, 1, 2
+#define SEGMENT_0D  B01000000
+#define SEGMENT_1D  B01111001
+#define SEGMENT_2D  B00100100
+#define SEGMENT_3D  B00110000
+#define SEGMENT_4D  B00011001
+#define SEGMENT_5D  B00010010
+#define SEGMENT_6D  B00000011
+#define SEGMENT_7D  B01111000
+#define SEGMENT_8D  B00000000
+#define SEGMENT_9D  B00011000
 
 #define SEGMENT_UNDER  B11110111
 #define SEGMENT_BAR	   B10111111
@@ -68,17 +63,32 @@ public:
 
 	byte Digits[MAX_DIGIT_COUNT];
 	
-	bool Setup(const uint8_t dataPin, const uint8_t clockPin, const uint8_t shiftPin, const uint8_t digitCount);
+	bool Setup(const byte dataPin, const byte clockPin, const byte shiftPin, const byte digitCount);
 
-	///Show the digits set from the number
-	void SetDigits(const uint32_t value, const bool show = true);
-
-	///Show the digits set from the number, omitting the leftmost zeros
-	void SetDigitsReplaceMSZero(const uint32_t value, const byte replaceZero, const bool show = true);
-	
+	//Set all digits to the same value.
 	void SetDigitsAll(const byte value);
-	uint32_t GetLastNumber();
-	void ShowDigits();
+
+	//Show the digits set from the set number.
+	void ShowNumber();
+
+	//Set the digits from the number, omitting the leftmost zeros by default.
+	//Returns true if the number has changed since last time.
+	bool SetNumber(const uint32_t number, const bool replaceZero = true);
+
+	//Get the last set number.
+	byte GetLastNumber();
+
+	//Set all outputs to the same bit value
+	void SetAll(const bool bitValue);
+
+	//Display the bytes in the provided array
+	void ShowBytes(byte byteArray[], byte byteArraySize);
+
+	//Translate single digit value to representation in a 7-segment byte.
+	byte GetDigitByte(const byte number1to9, const bool decimalOn = false);
+
+	//Clear display
+	void Clear();
 };
 
 #endif
